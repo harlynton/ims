85aa49f2f1ad21c371dc0b82fa4dd9db69691cc0
Harlynton <harlynton.castano@gmail.com>
2023-04-24 11:56:45 -0500

    Initial commit

---
+++ src/main/java/co/com/codecraft/ims/ImsInventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package co.com.codecraft.ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsInventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsInventoryManagementSystemApplication.class, args);
+	}
+
+}
